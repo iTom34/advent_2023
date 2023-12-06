c99d0be5df05897cff1c405865646b553482f976
Thomas Gaime <thomasgaime@gmail.com>
2023-12-06 21:28:20 +0100

    D6P1

---
+++ advent/day_5.py
@@ -0,0 +1,58 @@
+
+ACCELERATION_RATE: int = 1
+
+
+class Race:
+    def __init__(self, race_duration: int, previous_record: int):
+        self.race_duration: int = race_duration
+        self.previous_record: int = previous_record
+
+    def get_all_distance(self) -> dict[int: int]:
+        """
+        Returns a dictionary {time_pressed: distance_made}
+        """
+        result = {}
+        for button_pressed_duration in range(self.race_duration + 1):
+            result[button_pressed_duration] = self.get_distance_made(button_pressed_duration)
+
+        return result
+
+    def get_all_beating_records(self) -> dict[int: int]:
+        """
+        Returns a dictionary containing the time pressed that beat the record
+        {time_pressed: distance_made,...}
+        """
+        result = {}
+        all_distance: dict = self.get_all_distance()
+
+        for button_pressed_duration, distance in all_distance.items():
+            if distance > self.previous_record:
+                result[button_pressed_duration] = distance
+
+        return result
+
+    def get_number_of_ways_to_beat_record(self) -> int:
+        """
+        Return the number of ways to beat the record
+        """
+        return len(self.get_all_beating_records())
+
+    def get_distance_made(self, button_pressed_duration: int) -> int:
+        """
+        Computes the distance made
+        """
+        if button_pressed_duration >= self.race_duration:
+            return 0
+
+        racing_time = self.race_duration - button_pressed_duration
+        speed = button_pressed_duration * ACCELERATION_RATE
+
+        return racing_time * speed
+
+
+def puzzle_1(races: list[Race]):
+    product = 1
+    for race in races:
+        product *= race.get_number_of_ways_to_beat_record()
+
+    return product
---
+++ advent/tests/test_day_5.py
@@ -0,0 +1,105 @@
+import pytest
+
+from advent.day_5 import ACCELERATION_RATE, Race, puzzle_1, puzzle_2
+
+
+@pytest.fixture
+def r1_example() -> Race:
+    return Race(7, 9)
+
+
+@pytest.fixture
+def r2_example() -> Race:
+    return Race(15, 40)
+
+
+@pytest.fixture
+def r3_example() -> Race:
+    return Race(30, 200)
+
+
+@pytest.fixture
+def r1() -> Race:
+    return Race(55, 401)
+
+
+@pytest.fixture
+def r2() -> Race:
+    return Race(99, 1485)
+
+
+@pytest.fixture
+def r3() -> Race:
+    return Race(97, 2274)
+
+
+@pytest.fixture
+def r4() -> Race:
+    return Race(93, 1405)
+
+
+@pytest.fixture
+def races_example(r1_example, r2_example, r3_example) -> list[Race]:
+    return [r1_example, r2_example, r3_example]
+
+
+@pytest.fixture
+def races(r1, r2, r3, r4) -> list[Race]:
+    return [r1, r2, r3, r4]
+
+
+@pytest.fixture
+def f_puzzle_2_example() -> Race:
+    return Race(71530, 940200)
+
+
+@pytest.fixture
+def f_puzzle_2() -> Race:
+    return Race(55999793, 401148522741405)
+
+
+@pytest.mark.parametrize("button_pressed_duration, expected", [(0, 0),
+                                                               (1, 6),
+                                                               (2, 10),
+                                                               (3, 12),
+                                                               (4, 12),
+                                                               (5, 10),
+                                                               (6, 6),
+                                                               (7, 0),
+                                                               (8, 0)])
+def test_get_distance_made(r1_example: Race, button_pressed_duration: int, expected: int):
+    assert r1_example.get_distance_made(button_pressed_duration) == expected
+
+
+def test_get_all_distance(r1_example):
+    assert r1_example.get_all_distance() == {0: 0,
+                                             1: 6,
+                                             2: 10,
+                                             3: 12,
+                                             4: 12,
+                                             5: 10,
+                                             6: 6,
+                                             7: 0}
+
+
+def test_get_all_beating_records(r1_example):
+    assert r1_example.get_all_beating_records() == {2: 10,
+                                                    3: 12,
+                                                    4: 12,
+                                                    5: 10}
+
+
+@pytest.mark.parametrize("fixture_name, expected", [("r1_example", 4),
+                                                    ("r2_example", 8),
+                                                    ("r3_example", 9)])
+def test_get_number_of_ways_to_beat_record(fixture_name: str, expected: int, request):
+    race = request.getfixturevalue(fixture_name)
+    assert race.get_number_of_ways_to_beat_record() == expected
+
+
+@pytest.mark.parametrize("fixture_name, expected", [("races_example", 288),
+                                                    ("races", 2374848)])
+def test_puzzle_1(fixture_name: str, expected: int, request):
+    races = request.getfixturevalue(fixture_name)
+    assert puzzle_1(races) == expected
+
